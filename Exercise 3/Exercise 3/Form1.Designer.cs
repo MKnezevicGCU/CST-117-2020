c08d5b506b56d4d9b2c1c11eed4ddeac97f154e2
MKnezevicGCU <38193678+MKnezevicGCU@users.noreply.github.com>
2020-11-28 11:40:27 -0600

    Exercise 3

---
+++ Exercise 3/Exercise 3/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Exercise_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.convertButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.poundsLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.poundsTextBox = new System.Windows.Forms.TextBox();
+            this.kilogramLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(93, 127);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(75, 23);
+            this.convertButton.TabIndex = 0;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(218, 127);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // poundsLabel
+            // 
+            this.poundsLabel.AutoSize = true;
+            this.poundsLabel.Location = new System.Drawing.Point(53, 52);
+            this.poundsLabel.Name = "poundsLabel";
+            this.poundsLabel.Size = new System.Drawing.Size(141, 13);
+            this.poundsLabel.TabIndex = 2;
+            this.poundsLabel.Text = "Enter your weight in pounds:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "This is your weight in Kilograms:";
+            // 
+            // poundsTextBox
+            // 
+            this.poundsTextBox.Location = new System.Drawing.Point(215, 45);
+            this.poundsTextBox.Name = "poundsTextBox";
+            this.poundsTextBox.Size = new System.Drawing.Size(100, 20);
+            this.poundsTextBox.TabIndex = 4;
+            // 
+            // kilogramLabel
+            // 
+            this.kilogramLabel.Location = new System.Drawing.Point(215, 77);
+            this.kilogramLabel.Name = "kilogramLabel";
+            this.kilogramLabel.Size = new System.Drawing.Size(100, 23);
+            this.kilogramLabel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 255);
+            this.Controls.Add(this.kilogramLabel);
+            this.Controls.Add(this.poundsTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.poundsLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.convertButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label poundsLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox poundsTextBox;
+        private System.Windows.Forms.Label kilogramLabel;
+    }
+}
+
---
+++ Exercise 3/Exercise 3/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            double pounds;    //Declaring pounds as a variable
+            double kilograms; //Declaring kilograms as a variable
+            double answer;    //Declaring the answer as a variable
+
+            //Get the pounds from the text box.
+            pounds = double.Parse(poundsTextBox.Text);
+
+            //Set the kilograms
+            kilograms = 0.45359237;
+
+            //Calculate the pounds to kilograms
+            answer = pounds * kilograms;
+
+            //Display the answer in the label for answers.
+            kilogramLabel.Text = answer.ToString("n4");
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //Close the form.
+            this.Close();
+        }
+    }
+}
