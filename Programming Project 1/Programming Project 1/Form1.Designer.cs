72ea87c74fdf907836f2c9848472c027aa83097b
MKnezevicGCU <38193678+MKnezevicGCU@users.noreply.github.com>
2020-11-29 07:07:56 -0600

    made some changes to make it look nicer.

--- Programming Project 1/Programming Project 1/Form1.Designer.cs
+++ Programming Project 1/Programming Project 1/Form1.Designer.cs
@@ -29,13 +29,107 @@ namespace Programming_Project_1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.poundsLabel = new System.Windows.Forms.Label();
+            this.kilogramLabel = new System.Windows.Forms.Label();
+            this.poundsTextBox = new System.Windows.Forms.TextBox();
+            this.kilogramsOutputLabel = new System.Windows.Forms.Label();
+            this.ConvertButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // poundsLabel
+            // 
+            this.poundsLabel.AutoSize = true;
+            this.poundsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.poundsLabel.Location = new System.Drawing.Point(16, 57);
+            this.poundsLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.poundsLabel.Name = "poundsLabel";
+            this.poundsLabel.Size = new System.Drawing.Size(256, 16);
+            this.poundsLabel.TabIndex = 0;
+            this.poundsLabel.Text = "How many pounds do you wish to convert:";
+            // 
+            // kilogramLabel
+            // 
+            this.kilogramLabel.AutoSize = true;
+            this.kilogramLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kilogramLabel.Location = new System.Drawing.Point(76, 102);
+            this.kilogramLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.kilogramLabel.Name = "kilogramLabel";
+            this.kilogramLabel.Size = new System.Drawing.Size(196, 16);
+            this.kilogramLabel.TabIndex = 1;
+            this.kilogramLabel.Text = "This is your weight in Kilograms:";
+            // 
+            // poundsTextBox
+            // 
+            this.poundsTextBox.Location = new System.Drawing.Point(280, 51);
+            this.poundsTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.poundsTextBox.Name = "poundsTextBox";
+            this.poundsTextBox.Size = new System.Drawing.Size(170, 22);
+            this.poundsTextBox.TabIndex = 2;
+            // 
+            // kilogramsOutputLabel
+            // 
+            this.kilogramsOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.kilogramsOutputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kilogramsOutputLabel.Location = new System.Drawing.Point(280, 90);
+            this.kilogramsOutputLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.kilogramsOutputLabel.Name = "kilogramsOutputLabel";
+            this.kilogramsOutputLabel.Size = new System.Drawing.Size(170, 28);
+            this.kilogramsOutputLabel.TabIndex = 3;
+            this.kilogramsOutputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConvertButton.Location = new System.Drawing.Point(55, 164);
+            this.ConvertButton.Margin = new System.Windows.Forms.Padding(4);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(100, 28);
+            this.ConvertButton.TabIndex = 4;
+            this.ConvertButton.Text = "convert";
+            this.ConvertButton.UseVisualStyleBackColor = true;
+            this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitButton.Location = new System.Drawing.Point(322, 164);
+            this.ExitButton.Margin = new System.Windows.Forms.Padding(4);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(100, 28);
+            this.ExitButton.TabIndex = 5;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(467, 209);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.ConvertButton);
+            this.Controls.Add(this.kilogramsOutputLabel);
+            this.Controls.Add(this.poundsTextBox);
+            this.Controls.Add(this.kilogramLabel);
+            this.Controls.Add(this.poundsLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Convert Pounds to Kilograms";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label poundsLabel;
+        private System.Windows.Forms.Label kilogramLabel;
+        private System.Windows.Forms.TextBox poundsTextBox;
+        private System.Windows.Forms.Label kilogramsOutputLabel;
+        private System.Windows.Forms.Button ConvertButton;
+        private System.Windows.Forms.Button ExitButton;
     }
 }
 
--- Programming Project 1/Programming Project 1/Form1.cs
+++ Programming Project 1/Programming Project 1/Form1.cs
@@ -16,5 +16,38 @@ namespace Programming_Project_1
         {
             InitializeComponent();
         }
+
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double pounds;    //Declaring pounds as a variable
+                double kilograms; //Declaring kilograms as a variable
+                double answer;    //Declaring the answer as a variable
+
+                //Get the pounds from the text box.
+                pounds = double.Parse(poundsTextBox.Text);
+
+                //Set the kilograms
+                kilograms = 0.45359237;
+
+                //Calculate the pounds to kilograms
+                answer = pounds * kilograms;
+
+                //Display the answer in the label for answers.
+                kilogramsOutputLabel.Text = answer.ToString("n3");
+            }
+            catch (Exception ex)
+            {
+                //Display the default error message.
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            //Close the form.
+            this.Close();
+        }
     }
 }
