56f4360f4de4efda28ad73ba1fb3661cc8dfb272
MKnezevicGCU <38193678+MKnezevicGCU@users.noreply.github.com>
2020-12-02 18:20:51 -0600

    Exercise 9 finished

---
+++ Exercise 7/Exercise 7/Stubbing class.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7
+{
+    class Stubbing_class
+    {
+        private int Sum(int num1, int num2)
+        {
+            int result = 0;
+            ///
+            //result = num1 + num2;
+            return result;
+        }
+
+        private double Average(double num1, double num2, double num3, double num4, double num5)
+        {
+            
+        }
+
+        private int Sum(int num1, int num2)
+        {
+
+        }
+
+        private int Sum(int num1, int num2, int num3)
+        {
+
+        }
+        private string DisplayLessCharacters(string name, string age)
+        {
+
+        }
+        private List<double> LargestArrayValue(double[] num1, double[] num2)
+        {
+
+        }
+        private List<int> ReturnArray()
+        {
+            List<int> result = new List<int>();
+            for( int i =0; i < 50; i++)
+            {
+                result.Add(i);
+            }
+            return result;
+        }
+        private bool ReturnBoolVariable(int num1, int num2)
+        {
+
+        }
+        private int double ReturnAnswer(int num1, double num2)
+        {
+
+        }
+        private int AverageArray(int[] num1, int[] num2, int[] num3)
+        {
+
+        }
+    }
+}
---
+++ Exercise 9/Exercise 9/Program.cs
@@ -0,0 +1,50 @@
+﻿//use for IC08
+//Michael Knezevic's code
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //make some sets
+            Set A = new Set();
+            Set B = new Set();
+
+            //put some stuff in the sets
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                A.addElement(r.Next(4));
+                B.addElement(r.Next(12));
+            }
+
+            foreach(int item in A.Items())
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (int item in B.Items())
+            {
+                Console.WriteLine(item);
+            }
+
+            //display each set and the union
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+            Console.WriteLine("A union B: " + A.union(B));
+
+            //display original sets (should be unchanged)
+            Console.WriteLine("After union operation");
+            Console.WriteLine("A: " + A);
+            Console.WriteLine("B: " + B);
+
+        }
+    }
+}
