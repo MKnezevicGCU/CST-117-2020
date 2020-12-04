e5a032888cbd1576f1042f2873a09b358c0e3efc
MKnezevicGCU <38193678+MKnezevicGCU@users.noreply.github.com>
2020-12-03 19:49:32 -0600

    Exercise 10 and programming project 4

---
+++ Exercise 10/Exercise 10/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercise_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void searchText_Click(object sender, EventArgs e)
+        {
+            if (this.textBox1.Text.Contains("t"))
+            {
+                int countT = this.textBox1.Text.Split('t').Length - 1;
+                MessageBox.Show("Contains T=" + countT);
+            }
+                
+            if (this.textBox1.Text.Contains("e"))
+            {
+                int countE = this.textBox1.Text.Split('e').Length - 1;
+                MessageBox.Show("Contains E=" + countE);
+            }
+        }
+    }
+}
---
+++ Programming Project 4/Programming Project 4/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programming_Project_4
+{
+    public partial class Form1 : Form
+    {
+        bool isMyTurn = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.button1.Text.Contains("X") || this.button1.Text.Contains("O"))
+            {
+                return;
+            }
+            this.button1.Text = isMyTurn ? "X" : "O";
+            isMyTurn = !isMyTurn;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.button2.Text.Contains("X") || this.button2.Text.Contains("O"))
+            {
+                return;
+            }
+            this.button2.Text = isMyTurn ? "X" : "O";
+            isMyTurn = !isMyTurn;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (this.button3.Text.Contains("X") || this.button3.Text.Contains("O"))
+            {
+                return;
+            }
+            this.button3.Text = isMyTurn ? "X" : "O";
+            isMyTurn = !isMyTurn;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (this.button4.Text.Contains("X") || this.button4.Text.Contains("O"))
+            {
+                return;
+            }
+            this.button4.Text = isMyTurn ? "X" : "O";
+            isMyTurn = !isMyTurn;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (this.button5.Text.Contains("X") || this.button5.Text.Contains("O"))
+            {
+                return;
+            }
+            this.button5.Text = isMyTurn ? "X" : "O";
+            isMyTurn = !isMyTurn;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (this.button6.Text.Contains("X") || this.button6.Text.Contains("O"))
+            {
+                return;
+            }
+            this.button6.Text = isMyTurn ? "X" : "O";
+            isMyTurn = !isMyTurn;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (this.button7.Text.Contains("X") || this.button7.Text.Contains("O"))
+            {
+                return;
+            }
+            this.button7.Text = isMyTurn ? "X" : "O";
+            isMyTurn = !isMyTurn;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (this.button8.Text.Contains("X") || this.button8.Text.Contains("O"))
+            {
+                return;
+            }
+            this.button8.Text = isMyTurn ? "X" : "O";
+            isMyTurn = !isMyTurn;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (this.button9.Text.Contains("X") || this.button9.Text.Contains("O"))
+            {
+                return;
+            }
+            this.button9.Text = isMyTurn ? "X" : "O";
+            isMyTurn = !isMyTurn;
+        }
+    }
+}
